3b18a15877873d9b6ccee28399d0845db04d57dc
Tomasz Bawor <bawortomasz@gmail.com>
2020-02-28 11:46:01 +0100

    Adding styles for stream creation

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,15 +2,24 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput = ({ input }) => {
-    return <input onChange={input.onChange} value={input.value} />;
+  renderInput = ({ input, label }) => {
+    return (
+      <div className="field">
+        <label htmlFor={label}>{label}</label>
+        <input onChange={input.onChange} value={input.value} id={label} />
+      </div>
+    );
   };
 
   render() {
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <form className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
       </form>
     );
   }
